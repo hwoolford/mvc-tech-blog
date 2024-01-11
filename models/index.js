62ba9da39316505859d5963e2be6a3267c65291e
forxtoisate <hwoolford@gmail.com>
2024-01-11 15:09:55 -0600

    Updated model index with Blog

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 const User = require('./User');
-// const Blog = require('./Blog');
+const Blog = require('./Blog')
 
-// add any associations here (hasMany, belongsTo, etc)
+// User belongs to many blogs
+
+// Blog belongs to one User
 
 
 module.exports = { User, Blog }; //add other models here separated by a comma and a space
