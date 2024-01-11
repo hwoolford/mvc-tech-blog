5509c802fd39d5867784662d607fd194f968f873
forxtoisate <hwoolford@gmail.com>
2024-01-11 16:42:09 -0600

    Added routes for editing and deleting blog post

--- controllers/api/blog-routes.js
+++ controllers/api/blog-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const { Blog } = require('../../models');
+const { Blog, User } = require('../../models');
 
-// Create Blog Post
+// Create Blog Post Route
 router.post('/', async (req, res) => {
     try {
         const dbBlogData = await Blog.create({
@@ -13,7 +13,7 @@ router.post('/', async (req, res) => {
             res.status(400).json({ message: 'Please enter a title and content'});
             return;
         }
-        
+
         req.session.save(() => {
             req.session.loggedIn = true;
             res.status(200).json({ message: 'Thank you for posting!'});
@@ -21,6 +21,45 @@ router.post('/', async (req, res) => {
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Unable to post a blog at this time. Please try again later.'});
+    }
+});
+
+// Update (edit) Blog Post by Id Route
+router.put('/:id', async (req, res) => {
+    try {
+        const dbBlogData = await Blog.update(req.body, {
+            include: [{ model: User}],
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (dbBlogData[0] === 0) {
+            res.status(404).json({ message: 'No blog with this id'});
+            return;
+        }
+        res.status(200).json(dbBlogData);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
+// Delete Blog Post by Id Route
+router.delete('/:id', async (req, res) => {
+    try {
+        const dbBlogData = await Blog.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        if (!dbBlogData) {
+            res.status(404).json({ message: 'No blog with this id'});
+            return;
+        }
+        res.status(200).json(dbBlogData);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
     }
 });
 
