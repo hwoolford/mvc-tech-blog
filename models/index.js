8161a59591c54d2d3c9fc33c82028ff38ca3f65b
forxtoisate <hwoolford@gmail.com>
2024-01-11 15:14:07 -0600

    Updated model index with associations between User and Blog

--- models/index.js
+++ models/index.js
@@ -2,8 +2,13 @@ const User = require('./User');
 const Blog = require('./Blog')
 
 // User belongs to many blogs
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+})
 
 // Blog belongs to one User
+Blog.belongsTo(User, {
+    foreignKey: 'user_id',
+})
 
-
-module.exports = { User, Blog }; //add other models here separated by a comma and a space
+module.exports = { User, Blog }; 
