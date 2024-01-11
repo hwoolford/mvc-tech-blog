bbbae0afce2c689f355a947893de8fd1273c04b5
forxtoisate <hwoolford@gmail.com>
2024-01-11 14:49:33 -0600

    removed an unneeded require from User model and added it to the index

--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
-const { STRING } = require('sequelize');
 
 class User extends Model {
     checkPassword(loginPw) {
---
+++ models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+
+// add any associations here (hasMany, belongsTo, etc)
+
+
+module.exports = { User }; //add other models here separated by a comma and a space
