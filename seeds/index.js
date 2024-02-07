8a4f0f2fa379bb2778b450d7c5adac9732dd3893
forxtoisate <hwoolford@gmail.com>
2024-02-07 14:51:57 -0600

    Updated seeds/index.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,12 +1,34 @@
 const sequelize = require('../config/connection');
-const seedUser = require('./userData');
-const seedBlog = require('./blogData');
+const { User, Blog } = require('../models');
+
+const seedUser = require('./userData.json');
+const seedBlog = require('./blogData.json');
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
-    await seedUser();
-    await seedBlog();
+
+    const users = await User.bulkCreate(seedUser, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    for (const blog of seedBlog) {
+        await Blog.create({
+            ...blog,
+            user_id: users[Math.floor(Math.random() * users.length)].id,
+        });
+    }
+
     process.exit(0);
 };
 
 seedAll();
+
+// const seedAll = async () => {
+//     await sequelize.sync({ force: true });
+//     await seedUser();
+//     await seedBlog();
+//     process.exit(0);
+// };
+
+// seedAll();
